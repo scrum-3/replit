e9b83f76b7fb1e6afd9621b0bcd59e837c4e5fb4
scrum-3 <scrum3@mde.tw>
2025-06-18 03:36:56 +0000

    完成初步 Replit 可用性測試

--- init.py
+++ init.py
@@ -10,7 +10,7 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "cmsite"
+    site_title = "Replit 可用性測試"
     ip = "127.0.0.1"
     dynamic_port = 9445
     static_port = 8445
